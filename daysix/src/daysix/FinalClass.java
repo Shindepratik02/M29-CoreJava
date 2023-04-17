5ac42af6a612d249377127f8baee6d653938fd45
Pratik <Pratik@LAPTOP-9GJ0492B>
2023-04-17 20:09:01 +0530

    Day six commit

---
+++ daysix/src/daysix/FinalClass.java
@@ -0,0 +1,9 @@
+package daysix;
+
+final public class FinalClass {
+	
+	public void show() {
+		System.out.println("Final class cant be inherited");
+	}
+
+}
---
+++ daysix/src/daysix/FinalMethod.java
@@ -0,0 +1,17 @@
+package daysix;
+
+public class FinalMethod {
+
+	public FinalMethod() {
+		System.out.println("Default constructor");
+		// TODO Auto-generated constructor stub
+	}
+	final float salary=27000.85f;
+	final void show() {
+		System.out.println("Salary is "+salary);
+		
+	}
+	
+	
+
+}
