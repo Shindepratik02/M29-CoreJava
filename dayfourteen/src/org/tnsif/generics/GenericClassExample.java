f7b1edc8f4baa6e6f67e8f9fea0cc23e80db1eb6
Pratik <Pratik@LAPTOP-9GJ0492B>
2023-06-14 14:06:36 +0530

    Day fourteen commit

---
+++ dayfourteen/src/org/tnsif/generics/GenericClassExample.java
@@ -0,0 +1,26 @@
+package org.tnsif.generics;
+
+class GenericClass<T> {
+	
+	T x;
+	void print(T x)
+	{
+		this.x=x;
+	}
+	T accept()
+	{
+		return x;
+	}
+}
+public class GenericClassExample
+{
+	public static void main(String[] args) {
+		GenericClass<String>obj=new GenericClass<>();
+		obj.print("prabal");
+		System.out.println(obj.accept());
+		
+		
+
+	}
+
+}
