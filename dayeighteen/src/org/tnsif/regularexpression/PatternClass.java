6a650758386f6b775a963fbb04d0a1616812ddc0
Pratik <Pratik@LAPTOP-9GJ0492B>
2023-06-14 14:07:49 +0530

    Day eighteen commit

---
+++ dayeighteen/src/org/tnsif/regularexpression/PatternClass.java
@@ -0,0 +1,21 @@
+package org.tnsif.regularexpression;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class PatternClass {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the input string");
+		String input=s.nextLine();
+		String pattern="Hello Regular Expression";
+		boolean result=Pattern.matches(pattern, input);
+		System.out.println(result);
+		
+		
+		
+
+	}
+
+}
